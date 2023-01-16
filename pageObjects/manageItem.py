d20e14f1ce150465744af32bfa0b77d38857ec40
shahzaman-mehdi <shahzaman.mehdi12@gmail.com>
2023-01-16 18:12:10 +0500

    Fourth commit

--- pageObjects/manageItem.py
+++ pageObjects/manageItem.py
@@ -1,12 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-class ItemManagement:
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
+
+class ItemManagement:
     list_advanceInventory_linktxt = "Advance Inventory"
-    link_itemMasterNav_xpath = '//*[@id="panel_3124"]/li[2]/a'
+    link_itemMasterNav_linktxt = 'Item Master'
     button_addItem_xpath = '//*[@id="headingRow"]/tbody/tr/td[1]/a'
-    list_itemCategory_dropdown = '//*[@id="icform"]/div[2]/div[1]/span[2]/span/span[2]'
-    link_itemCategoryValue_linktxt = "PHONE"
+    textbox_itemCode_xpath = "//input[@id='ItemCode']"
+    textbox_itemName_id = "ItemName"
+    textbox_itemDescription_xpath = "//input[@id='ItemDescription']"
+    textbox_upcNumber_xpath = "//input[@id='UPCCode']"
+    textbox_standardCost_xpath = "//input[@id='Item_Weight_LB']"
+    textbox_sellingPrice_id = "UnitPrice"
+    button_saveItem_id = 'btnContinueSave'
 
     def __init__(self, driver):
         self.driver = driver
@@ -15,16 +23,33 @@ class ItemManagement:
         self.driver.find_element(By.PARTIAL_LINK_TEXT, self.list_advanceInventory_linktxt).click()
 
     def clickItemMasterNav(self):
-        self.driver.find_element(By.XPATH, self.link_itemMasterNav_xpath).click()
+        self.driver.find_element(By.PARTIAL_LINK_TEXT, self.link_itemMasterNav_linktxt).click()
 
     def clickAddItem(self):
         self.driver.find_element(By.XPATH, self.button_addItem_xpath).click()
 
-    def clickCategoryLink(self):
-        self.driver.find_element(By.XPATH, self.list_itemCategory_dropdown).click()
+    def enterItemCode(self, code):
+        self.driver.find_element(By.XPATH, self.textbox_itemCode_xpath).clear()
+        self.driver.find_element(By.XPATH, self.textbox_itemCode_xpath).send_keys(code)
 
-    def selectItemCategoryValue(self):
-        self.driver.find_element(By.XPATH, self.link_itemCategoryValue_linktxt).click()
+    def enterItemName(self, name):
+        self.driver.find_element(By.ID, self.textbox_itemName_id).send_keys(name)
 
+    def enterItemDescription(self, description):
+        self.driver.find_element(By.XPATH, self.textbox_itemDescription_xpath).clear()
+        self.driver.find_element(By.XPATH, self.textbox_itemDescription_xpath).send_keys(description)
 
+    def enterUPCNumber(self, upc):
+        self.driver.find_element(By.XPATH, self.textbox_upcNumber_xpath).clear()
+        self.driver.find_element(By.XPATH, self.textbox_upcNumber_xpath).send_keys(upc)
 
+    def enterStandardCost(self, stdCost):
+        self.driver.find_element(By.XPATH, self.textbox_standardCost_xpath).click()
+        self.driver.find_element(By.XPATH, self.textbox_standardCost_xpath).send_keys(stdCost)
+
+    def enterSellingPrice(self, sellPrice):
+        self.driver.find_element(By.ID, self.textbox_sellingPrice_id).click()
+        self.driver.find_element(By.ID, self.textbox_sellingPrice_id).send_keys(sellPrice)
+
+    def clickSaveAndContinue(self):
+        self.driver.find_element(By.ID, self.button_saveItem_id).click()
--- testCases/test_add_NonSerialized_Item.py
+++ testCases/test_add_NonSerialized_Item.py
@@ -1,5 +1,8 @@
+import random
 import time
+import string
 import pytest
+from selenium import webdriver
 from pageObjects.manageItem import ItemManagement
 from pageObjects.LoginPage import LoginPage
 from Utilities.readProperties import ReadConfig
@@ -24,14 +27,69 @@ class Test_003_Add_NonSerialized_Item:
         self.lp.setUsername(self.username)
         self.lp.setPassword(self.password)
         self.lp.clickLogin()
-        self.logger.info("********** Login Successfull **********")
-
         self.lp.clickRemindLater()
-        self.ai = ItemManagement()
+        self.logger.info("********** Login Successful **********")
+
+        self.ai = ItemManagement(self.driver)
         self.ai.clickInventoryNav()
+        time.sleep(2)
+
         self.ai.clickItemMasterNav()
+        time.sleep(2)
+
         self.ai.clickAddItem()
-        self.ai.clickCategoryLink()
-        self.ai.selectItemCategoryValue()
+        time.sleep(2)
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(10)
 
 
+        # self.code = 'AT' + randomNumber()
+        # print(self.code)
+        # self.ai.enterItemCode(self.code)
+        # time.sleep(2)
+        #
+        # self.name = 'AT Item ' + randomNumber()
+        # print(self.name)
+        # self.ai.enterItemName(self.name)
+        # time.sleep(2)
+        #
+        # self.ai.enterItemDescription('This it the item created by automation testing. The test case id is '
+        #                              'Test_003_Add_NonSerialized_Item')
+        # # time.sleep(2)
+        #
+        # self.upc = 'UPC' + randomNumber()
+        # print(self.upc)
+        # self.ai.enterUPCNumber(self.upc)
+        # time.sleep(2)
+
+        self.ai.enterStandardCost(500)
+        time.sleep(2)
+
+        # self.ai.enterSellingPrice(1000)
+        # time.sleep(2)
+        #
+        # self.ai.clickSaveAndContinue()
+        # time.sleep(5)
+        #
+        # self.msg = self.driver.find_elements_by_tag_name("body").text
+        #
+        # if self.msg == 'Record Save Successfully':
+        #     assert True
+        # else:
+        #     assert False
+
+        # // *[ @ id = "htmlBody"] / div[3] / div / div / text()
+        # // div[ @
+        #
+        # class ='k-notification-wrap']
+        # // div[@ class ='k-notification-wrap']
+        # Record Save Successfully
+
+# def randomNumber():
+#     randNum = ''.join(random.choice(string.digits) for i in range(5))
+#     return randNum
+
+
+# def randomChar():
+#     randChar = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
+#     return randChar
