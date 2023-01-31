b9cffbe914cfd887d3d0b3784bc53ae8f1d72513
shahzaman-mehdi <shahzaman.mehdi12@gmail.com>
2023-01-31 23:58:51 +0500

    Add serialized item

--- pageObjects/manageItem.py
+++ pageObjects/manageItem.py
@@ -7,7 +7,7 @@ class ItemManagement:
     link_itemMasterNav_linktxt = 'Item Master'
     button_addItem_xpath = '//*[@id="headingRow"]/tbody/tr/td[1]/a'
     dropdown_itemCategory_xpath = "//*[@id='icform']/div[2]/div[1]/span[2]"
-    select_itemCategory_xpath = "//*[@id='ItemCategoryID_listbox']/li[4]"
+    select_itemCategory_xpath = "//*[@id='ItemCategoryID_listbox']/li[3]"
     textbox_itemCode_xpath = "//input[@id='ItemCode']"
     textbox_itemName_id = "ItemName"
     textbox_itemDescription_xpath = "//input[@id='ItemDescription']"
@@ -23,8 +23,21 @@ class ItemManagement:
     link_searchItemValue_xpath = '//*[@id="kGrid"]/table/tbody/tr/td[2]/a'
     button_updateItem_xpath = '//*[@id="icform"]/div[18]/div/button[1]'
 
-    #################### Edit Item Page Objects ############################
-    
+    #################### Serialized Item Page Objects ############################
+    dropdown_itemType_xpath = '//*[@id="icform"]/div[2]/div[3]/span[2]/span/span[2]'
+    dropdown_itemTypeValue_xpath = '//*[@id="ItemType_listbox"]/li[9]'
+    dropdown_itemMake_xpath = '//*[@id="icform"]/div[5]/div[1]/span/span/span[2]'
+    dropdown_makeValue_xpath = '//*[@id="sMake_listbox"]/li'
+    dropdown_itemBrand_xpath = '//*[@id="icform"]/div[5]/div[2]/span/span/span[2]'
+    dropdown_brandValue_xpath = '//*[@id="sBrand_listbox"]/li'
+    dropdown_itemModel_xpath = '//*[@id="icform"]/div[5]/div[3]/span/span/span[2]'
+    dropdown_modelValue_xpath = '//*[@id="sModel_listbox"]/li'
+    button_addItemMemory_xpath = '//*[@id="icform"]/div[5]/div[4]/a/span[2]'
+    textbox_addItemMemory_xpath = '//*[@id="sName"]'
+    button_saveItemMemory_xpath = '//*[@id="btn_Submit"]/span[2]'
+    dropdown_itemMemory_xpath = '//*[@id="icform"]/div[5]/div[4]/span/span/span[1]'
+    textbox_searchItemMemory_xpath = '//*[@id="sCapacity-list"]/span/input'
+    dropdown_memoryValue_xpath = '//*[@id="c511fa5f-7828-47ad-ad1d-f533c8515a02"]'
 
     def __init__(self, driver):
         self.driver = driver
@@ -59,9 +72,9 @@ class ItemManagement:
         self.driver.find_element(By.XPATH, self.textbox_upcNumber_xpath).clear()
         self.driver.find_element(By.XPATH, self.textbox_upcNumber_xpath).send_keys(upc)
 
-    def enterStandardCost(self, stdCost):
-        self.driver.find_element(By.XPATH, self.increase_value).click()
-        self.driver.find_element(By.XPATH, self.increase_value).send_keys(stdCost)
+    # def enterStandardCost(self, stdCost):
+    #     self.driver.find_element(By.XPATH, self.increase_value).click()
+    #     self.driver.find_element(By.XPATH, self.increase_value).send_keys(stdCost)
     #
     # def enterSellingPrice(self, sellPrice):
     #     self.driver.find_element(By.ID, self.textbox_sellingPrice_id).click()
@@ -83,3 +96,36 @@ class ItemManagement:
 
     def clickSaveAndClose(self):
         self.driver.find_element(By.XPATH, self.button_updateItem_xpath).click()
+
+    def selectItemType(self):
+       self.driver.find_element(By.XPATH, self.dropdown_itemType_xpath).click()
+       time.sleep(2)
+       self.driver.find_element(By.XPATH, self.dropdown_itemTypeValue_xpath).click()
+
+    def selectItemMake(self):
+        self.driver.find_element(By.XPATH, self.dropdown_itemMake_xpath).click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, self.dropdown_makeValue_xpath).click()
+
+    def selectItemBrand(self):
+        self.driver.find_element(By.XPATH, self.dropdown_itemBrand_xpath).click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, self.dropdown_brandValue_xpath).click()
+
+    def selectItemModel(self):
+        self.driver.find_element(By.XPATH, self.dropdown_itemModel_xpath).click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, self.dropdown_modelValue_xpath).click()
+
+    def addItemMemoryAtribute(self, memory):
+        self.driver.find_element(By.XPATH, self.button_addItemMemory_xpath).click()
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, self.textbox_addItemMemory_xpath).send_keys(memory)
+        self.driver.find_element(By.XPATH, self.button_saveItemMemory_xpath).click()
+
+    def selectItemMemory(self, memory):
+        self.driver.find_element(By.XPATH, self.dropdown_itemMemory_xpath).click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, self.textbox_searchItemMemory_xpath).send_keys(memory)
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, self.dropdown_memoryValue_xpath).click()
---
+++ testCases/test_add_Serialized_Item.py
@@ -0,0 +1,107 @@
+import random
+import time
+import string
+
+import pytest
+from selenium.webdriver.common.by import By
+from pageObjects.manageItem import ItemManagement
+from pageObjects.LoginPage import LoginPage
+from Utilities.readProperties import ReadConfig
+from Utilities.customLogger import LogGen
+
+
+class Test_003_Add_Serialized_Item:
+    baseURL = ReadConfig.getApplicationURL()
+    username = ReadConfig.getUserEmail()
+    password = ReadConfig.getPassword()
+
+    logger = LogGen.loggen()
+
+    @pytest.mark.sanity
+    @pytest.mark.regression
+    def test_addSerializedItem(self, setup):
+        self.logger.info("********** Test_003_Add_Serialized_Item **********")
+        self.logger.info("********** Verifying Add Serialized Item **********")
+
+        self.driver = setup
+        self.driver.maximize_window()
+        self.driver.get(self.baseURL)
+        self.lp = LoginPage(self.driver)
+        self.lp.setUsername(self.username)
+        self.lp.setPassword(self.password)
+        self.lp.clickLogin()
+        self.lp.clickRemindLater()
+        self.logger.info("********** Login Successful **********")
+
+        self.ai = ItemManagement(self.driver)
+        self.ai.clickInventoryNav()
+        time.sleep(2)
+
+        self.ai.clickItemMasterNav()
+        time.sleep(2)
+
+        self.ai.clickAddItem()
+        time.sleep(15)
+
+        # self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # time.sleep(5)
+
+        # self.ai.selectItemCategory()
+        # time.sleep(2)
+        #
+        # self.code = 'AT' + randomNumber()
+        # self.ai.enterItemCode(self.code)
+        # time.sleep(2)
+        #
+        # self.name = 'AT Item ' + randomNumber()
+        # self.ai.enterItemName(self.name)
+        # time.sleep(2)
+        #
+        # self.ai.enterItemDescription('This it the item created by automation testing. The test case id is '
+        #                              'Test_003_Add_NonSerialized_Item')
+        # time.sleep(2)
+        #
+        # self.upc = 'UPC' + randomNumber()
+        # self.ai.enterUPCNumber(self.upc)
+        # time.sleep(2)
+
+        self.ai.selectItemType()
+        self.ai.selectItemMake()
+        self.ai.selectItemBrand()
+        self.ai.selectItemModel()
+
+        self.memory = 'M' + randomNumber()
+        self.ai.addItemMemoryAtribute(self.memory)
+        self.ai.selectItemMemory(self.memory)
+
+        # self.cost = 500.12
+        # self.ai.enterStandardCost(self.cost)
+        # time.sleep(2)
+
+        # self.ai.enterSellingPrice(1000)
+        # time.sleep(2)
+        #
+        self.ai.clickSaveAndContinue()
+        self.logger.info("********** Saving **********")
+        time.sleep(10)
+
+        self.msg = self.driver.find_element(By.TAG_NAME, 'body').text
+
+        if 'Record Save Successfully' in self.msg:
+            assert True == True
+            self.logger.info("**********Item added successfully **********")
+            self.driver.close()
+        else:
+            self.driver.save_screenshot(".\\Screenshots\\" + "test-addNonSerializedItem_scr.png")
+            self.logger.error("**********Add Non Serialized Item Test Failed **********")
+            self.driver.close()
+            assert True == False
+
+
+def randomNumber():
+    randNum = ''.join(random.choice(string.digits) for i in range(5))
+    return randNum
+
+# def randomChar():
+#     randChar = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
+#     return randChar
